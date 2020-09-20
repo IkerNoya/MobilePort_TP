96c558034fbb273d856fc0ec501fcfc86e810579
Iker <ikernoya4@gmail.com>
2020-09-20 18:21:32 -0300

    Created new driving mechanic

--- Assets/ESCENAS/Game_1 Scripts/CameraManager.cs
+++ Assets/ESCENAS/Game_1 Scripts/CameraManager.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] GameObject player1; 
+    [SerializeField] GameObject player2;
+    [SerializeField] Vector3 offset;
+    [SerializeField] float followSpeed;
+    public enum PlayerCam
+    {
+        player1, player2
+    }
+    public PlayerCam player;
+
     void Start()
     {
-        
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        switch(player)
+        {
+            case PlayerCam.player1:
+                if(player1 != null)
+                {
+                    transform.position = Vector3.Lerp(transform.position, player1.transform.position + offset, followSpeed * Time.deltaTime);
+                    transform.LookAt(player1.transform.position);
+                }
+                break;
+            case PlayerCam.player2:
+                if (player2 != null)
+                {
+                    transform.position = Vector3.Lerp(transform.position, player2.transform.position + offset, Time.deltaTime);
+                    transform.LookAt(player1.transform.position);
+                }
+                break;
+        }
     }
 }
--- Assets/ESCENAS/Game_1 Scripts/TruckController.cs
+++ Assets/ESCENAS/Game_1 Scripts/TruckController.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class TruckController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] List<WheelCollider> throttleWheels;
+    [SerializeField] List<WheelCollider> steeringWheels;
+    [SerializeField] float strenghCoefficient;
+    [SerializeField] float brakeStrengh;
+    [SerializeField] float maxTurn;
+    [SerializeField] Transform centerMass;
+    [SerializeField] Rigidbody rb;
+    bool startBrake = false;
     void Start()
     {
-        
+        if(centerMass!=null)
+            rb.centerOfMass = centerMass.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (InputManager.Instance.GetAxis("Vertical") <= 0)
+        {
+            startBrake = true;
+        }
+        else if(InputManager.Instance.GetAxis("Vertical") > 0)
+        {
+            startBrake = false;
+        }
+    }
+    void FixedUpdate()
+    {
+        foreach(WheelCollider wheel in throttleWheels)
+        {
+            if (!startBrake)
+            {
+                wheel.motorTorque = strenghCoefficient * Time.deltaTime * InputManager.Instance.GetAxis("Vertical");
+                wheel.brakeTorque = 0f;
+            }
+            else
+            {
+                wheel.motorTorque = 0f;
+                wheel.brakeTorque = brakeStrengh;
+            }
+        }
+        foreach(WheelCollider wheel in steeringWheels)
+        {
+            wheel.steerAngle = maxTurn * InputManager.Instance.GetAxis("Horizontal");
+        }
     }
 }
