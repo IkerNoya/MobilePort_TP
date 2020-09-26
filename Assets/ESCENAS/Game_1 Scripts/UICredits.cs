fb182687a86052027a426efcd63345bce1c07c19
Iker <ikernoya4@gmail.com>
2020-09-26 20:50:31 -0300

    Created credits scenes

---
+++ Assets/ESCENAS/Game_1 Scripts/UICredits.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UICredits : MonoBehaviour
+{
+    public void OnClickMenu(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
